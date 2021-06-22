b76fc312308b758b40331072ccfa87bdda14ca62
Lena E <elena.evdokimova@softwire.com>
2021-06-22 15:51:01 +0100

    Working on burger-button.
    (Burger button is there, working on it to work)

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,36 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import '../styles/BurgerButton.scss'
+import {useState} from 'react';
 
 export function NavBar() {
+    
+    const [burgerState , setBurgerState] = useState(true);
+
+    function showBurgerMenu() {
+        
+        setBurgerState(!burgerState);
+
+
+
+    }
+
+
     return (
-        <div className='nav-bar'>
-            <Link to='/'> Home </Link>
-            <Link to='/spirit' >Spirit </Link>
-            <Link to='/opportunity'>Opportunity</Link>
-            <Link to='/curiosity'>Curiosity</Link>
-            <Link to='/timeline'> Timeline</Link>
+        <div className='header'>
+            <div className='nav-bar'>
+                <Link to='/'> Home </Link>
+                <Link to='/spirit' >Spirit </Link>
+                <Link to='/opportunity'>Opportunity</Link>
+                <Link to='/curiosity'>Curiosity</Link>
+                <Link to='/timeline'> Timeline</Link>
+            </div>
+
+            <div className="burger-menu-container" onClick={showBurgerMenu}>
+                <div className="bar-1"></div>
+                <div className="bar-2"></div>
+                <div className="bar-3"></div>
+            </div>
         </div>
     )
 }
