c7d9b6873cbd6ed4370372254e87c6dde8375011
Lena E <elena.evdokimova@softwire.com>
2021-06-22 20:56:58 +0100

    Burger button is closing on click and the NavBar appearing

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import '../styles/BurgerButton.scss'
-import {useState} from 'react';
+import { useState } from 'react';
 
 export function NavBar() {
-    
-    const [burgerState , setBurgerState] = useState(true);
 
-    function showBurgerMenu() {
-        
-        setBurgerState(!burgerState);
-
-
-
-    }
-
+    const [closeBurger, setBurgerState] = useState(true);
+    console.log(closeBurger);
 
     return (
         <div className='header'>
-            <div className='nav-bar'>
+            <div className={closeBurger ? 'nav-bar' : 'nav-bar-mobile'}>
                 <Link to='/'> Home </Link>
                 <Link to='/spirit' >Spirit </Link>
                 <Link to='/opportunity'>Opportunity</Link>
@@ -26,10 +18,12 @@ export function NavBar() {
                 <Link to='/timeline'> Timeline</Link>
             </div>
 
-            <div className="burger-menu-container" onClick={showBurgerMenu}>
-                <div className="bar-1"></div>
-                <div className="bar-2"></div>
-                <div className="bar-3"></div>
+            <div className="burger-menu-container" 
+                onClick={() => {setBurgerState(!closeBurger)}
+            }>
+                <div className={closeBurger ? "bar-1" : "change-bar-1"}></div>
+                <div className={closeBurger ? "bar-2" : "change-bar-2"}></div>
+                <div className={closeBurger ? "bar-3" : "change-bar-3"}></div>
             </div>
         </div>
     )
