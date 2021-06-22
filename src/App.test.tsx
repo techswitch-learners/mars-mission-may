f6f5f3ab9531ff7b413861ae3d4a982bd1c34b00
Oskar Williams <oskarwilliams378@gmail.com>
2021-06-22 10:20:39 +0100

    Fix hello world test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders hello world', () => {
+test('renders hello how are you', () => {
     render(<App />);
-    const helloWorldElement = screen.getByText("Hello World!");
+    const helloWorldElement = screen.getByText("Hello how are you?");
     expect(helloWorldElement).toBeInTheDocument();
 });
