fe948b3aba89d9b1743cd7cddb6f705de981029d
Ismat <ismat.mahmood@softwire.com>
2021-06-22 12:36:04 +0100

    Corrected filenames

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.scss';
-import { Home } from './components/Home';
+import { Home } from './components/home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
