da24f1deda371dd3eb6bbda4bd0215c1fc866bfb
Oskar Williams <oskarwilliams378@gmail.com>
2021-06-22 10:15:29 +0100

    Change hello world to hello how are you

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <h2>
-                    Hello World!
+                    Hello how are you?
                 </h2>
             </header>
         </div>
