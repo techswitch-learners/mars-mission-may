77c5af360075943b1224410417271ccbc88f7218
OPTIMIMCOW <kenallan18@hotmail.com>
2021-06-22 14:19:04 +0100

    Removed redundant import statements.

--- src/components/TimelinePage.jsx
+++ src/components/TimelinePage.jsx
@@ -1,6 +1,3 @@
-import { useEffect, useState } from 'react';
-
-
 export function TimelinePage() {
     return (
         <h1 className="title">Timeline</h1>
--- src/tests/TimelinePage.test.tsx
+++ src/tests/TimelinePage.test.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from '../App';
 import { TimelinePage } from '../components/TimelinePage.jsx';
 
 test('renders Timeline Page', () => {
