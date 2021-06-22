11b9fe44c4b2750b4e289486ae7de68d24cd49b8
Luke Stern <luke.stern@softwire.com>
2021-06-22 13:34:35 +0100

    New lines

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,9 +1,9 @@
 import { render, screen } from '@testing-library/react';
-
 import App from '../App';
 
 test('renders landing/home page', () => {
     render(<App />);
+    
     const navbarElement = screen.getByText("Replace with component for navbar");
     const weatherBarElement = screen.getByText("Replace with component for weather bar");
     const landingPageElement = screen.getByText("Replace with component for landing page");
