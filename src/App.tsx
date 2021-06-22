c362862449353645f91ec3873c025b62a6284905
Luke Stern <luke.stern@softwire.com>
2021-06-22 12:33:38 +0100

    Top level app route

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.scss';
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <h2>
-                    Hello how are you?
-                </h2>
-            </header>
-        </div>
+        <Router forceRefresh={true}>
+            <div>Replace with component for navbar</div>
+            <div>Replace with component for weather bar</div>
+            <Switch>
+                <Route exact path="/">
+                    <div>Replace with component for landing page</div>
+                </Route>
+                <Route exact path="/timeline">
+                    <div>Replace with component for timeline page</div>
+                </Route>
+                <Route exact path="/:rover">
+                    <div>Replace with component for rover page. :rover is a variable name either spirit, opportunity or curiosity. The component should load each page based off of the rover name.</div>
+                </Route>
+                <Route exact path="/:rover/:photoId">
+                    <div>Replace with component for photoId with the user selected photo, use the unique Id for each image from the api to replace the main content</div>
+                </Route>
+                <Route path="">
+                    <div>Sorry path not found</div>
+                </Route>
+            </Switch>
+            <div>Replace with component for footer</div>
+        </Router>
     );
 }
 
