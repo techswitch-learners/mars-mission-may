972bdff84accc3ecb037cb7be0e8cae162c08373
OPTIMIMCOW <kenallan18@hotmail.com>
2021-06-22 13:58:31 +0100

    Made a change for fileName update to PascalCase

--- src/tests/timelinePage.test.tsx
+++ src/tests/timelinePage.test.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
-import { TimelinePage } from '../components/timelinePage.jsx';
+import { TimelinePage } from '../components/TimelinePage.jsx';
 
 test('renders Timeline Page', () => {
     render(<TimelinePage />);
-    const helloWorldElement = screen.getByText("Timeline");
+    const helloWorldElement = screen.getByText("Timeline - Change for git");
     expect(helloWorldElement).toBeInTheDocument();
 });
