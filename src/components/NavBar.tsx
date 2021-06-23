f9a2ff035d8782bbbd0f30e62a3c99897ce8991a
Cress <cressida.chesworth@softwire.com>
2021-06-23 14:18:14 +0100

    Created Burger Button + menu + tests

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,26 +4,25 @@ import '../styles/BurgerButton.scss'
 import { useState } from 'react';
 
 export function NavBar() {
-
     const [closeBurger, setBurgerState] = useState(true);
-    console.log(closeBurger);
 
     return (
         <div className='header'>
-            <div className={closeBurger ? 'nav-bar' : 'nav-bar-mobile'}>
-                <Link to='/'> Home </Link>
-                <Link to='/spirit' >Spirit </Link>
-                <Link to='/opportunity'>Opportunity</Link>
-                <Link to='/curiosity'>Curiosity</Link>
-                <Link to='/timeline'> Timeline</Link>
+            <div className={closeBurger ? 'nav-bar' : 'nav-bar-mobile'} data-testid="nav-bar">
+                <Link to='/' className = 'nav-link'> Home </Link>
+                <Link to='/spirit' className = 'nav-link'>Spirit </Link>
+                <Link to='/opportunity'className = 'nav-link'>Opportunity</Link>
+                <Link to='/curiosity'className = 'nav-link'>Curiosity</Link>
+                <Link to='/timeline'className = 'nav-link' > Timeline</Link>
             </div>
 
-            <div className="burger-menu-container" 
+            <div className="burger-button" 
                 onClick={() => {setBurgerState(!closeBurger)}
-            }>
-                <div className={closeBurger ? "bar-1" : "change-bar-1"}></div>
-                <div className={closeBurger ? "bar-2" : "change-bar-2"}></div>
-                <div className={closeBurger ? "bar-3" : "change-bar-3"}></div>
+            } data-testid= 'burger-test'>
+                <div className={closeBurger ? "opened-bar-1" : "closed-bar-1"} 
+                    data-testid={closeBurger ? "opened-bar-1" : "closed-bar-1"}></div>
+                <div className={closeBurger ? "opened-bar-2" : "closed-bar-2"}></div>
+                <div className={closeBurger ? "opened-bar-3" : "closed-bar-3"}></div>
             </div>
         </div>
     )
