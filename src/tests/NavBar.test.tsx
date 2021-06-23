34d2a53b153d391bebecacd4d5179cd0754c1a10
Stewart <Stewart.bowie@softwire.com>
2021-06-23 15:42:23 +0100

    Fix Lint issues and more merge conflicts

--- src/tests/NavBar.test.tsx
+++ src/tests/NavBar.test.tsx
@@ -1,6 +1,7 @@
-import { render, screen } from '@testing-library/react';
-import App from '../App';
-import userEvent from '@testing-library/user-event';
+import { render, screen } from "@testing-library/react";
+import App from "../App";
+import userEvent from "@testing-library/user-event";
+import React from "react";
 
 test("renders navbar", () => {
     render(<App />);
@@ -17,14 +18,14 @@ test("renders navbar", () => {
     expect(timeline).toBeInTheDocument();
 });
 
-test('burger button exists', () => {
+test("burger button exists", () => {
     render(<App />);
     const burgerButton = screen.getByTestId("burger-test");
 
     expect(burgerButton).toBeInTheDocument();
 });
 
-test('burger button click changes testId of bars', () => {
+test("burger button click changes testId of bars", () => {
     render(<App />);
     const burgerButton = screen.getByTestId("burger-test");
     userEvent.click(burgerButton); 
