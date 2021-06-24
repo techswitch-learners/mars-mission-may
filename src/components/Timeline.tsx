68f317e90ee0cfc5c38ce221bf35d11f94c2acc9
Ismat <ismat.mahmood@softwire.com>
2021-06-24 12:11:02 +0100

    Styling added but not completed as waiting for card component

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,10 +1,31 @@
-import { timelineData } from "./timelineData";
+import { timelineData, timelineHeaderData } from "./timelineData";
+import "../styles/constants.scss";
+import "../styles/timeline.scss";
 
 export function Timeline() {
-    return (
+
+    const map = timelineData.map((post) => (
         <div>
-            <h1 className="title">Timeline</h1>
-            <p>{timelineData[0].title}</p>
+            <h3>{post.launchDate}</h3>
+            <h3>{post.title}</h3>
+            <h3>{post.status}</h3>
+            <p>{post.description}</p>
+            <img alt="" src={post.imageUrl} />
+        </div>
+    ));
+
+    return (
+        <div id="timeline-page">
+            <div id="banner-img">
+                <img alt="" src="https://blogs.nasa.gov/redplanetdispatch/wp-content/uploads/sites/279/2018/03/Mars-Website-banner-1024x309.jpg" />
+            </div>
+            <div id="main-card">
+                <img alt="" src={timelineHeaderData.image} />
+                <p>{timelineHeaderData.text}</p>
+            </div>
+            <div className="cards">
+                {map}
+            </div>
         </div>
     );
 }
