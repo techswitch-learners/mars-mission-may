e8209e3ac04753280ff34d4badbcbbde5b4fbf13
OPTIMIMCOW <kenallan18@hotmail.com>
2021-06-24 22:57:56 +0100

    Styling changes to allow scale between desktop and 425px wide only.
    Changed card media rule from 425px to 600px
    Changed home media rule from 425px to 600px
    Simplified class naming.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,13 +6,13 @@ export function Home() {
     return (
         <div className="home">
             <img className="banner-image" src="https://blogs.nasa.gov/redplanetdispatch/wp-content/uploads/sites/279/2018/03/Mars-Website-banner-1024x309.jpg" />
-            <div className="home-card-holder">
+            <div className="card-holder">
                 <Card imageSrc={timelineData[2].imageUrl} title={timelineData[2].title} body={timelineData[2].mission as string} href="/curiosity" showInitialText={true} textHideable={false} imageOnRight={false} />
             </div>
-            <div className="home-card-holder">
+            <div className="card-holder">
                 <Card imageSrc={timelineData[3].imageUrl} title={timelineData[3].title} body={timelineData[3].mission as string} href="/opportunity" showInitialText={true} textHideable={false} imageOnRight={true} />
             </div>
-            <div className="home-card-holder">
+            <div className="card-holder">
                 <Card imageSrc={timelineData[4].imageUrl} title={timelineData[4].title} body={timelineData[4].mission as string} href="/spirit" showInitialText={true} textHideable={false} imageOnRight={false} />
             </div>
             <div className="footer-padding"></div>
