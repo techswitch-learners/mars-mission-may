7d43b90ca4d589c03fa6dd300cda63b0e6bca7ff
Ismat <ismat.mahmood@softwire.com>
2021-06-24 13:46:22 +0100

    Basic Styling and Header image added

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import "../styles/Home.scss"
 
 export function Home() {
     return (
-        <div>
+        <div id="Home">
+            <img src="https://blogs.nasa.gov/redplanetdispatch/wp-content/uploads/sites/279/2018/03/Mars-Website-banner-1024x309.jpg" />
             <h2>Home Page</h2>
         </div>
     );
