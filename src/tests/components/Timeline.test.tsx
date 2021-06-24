f02689ef1333cb9dacd15308b80dfa08adbda9a6
OPTIMIMCOW <kenallan18@hotmail.com>
2021-06-24 10:00:30 +0100

    Change to pass tests.

--- src/tests/components/Timeline.test.tsx
+++ src/tests/components/Timeline.test.tsx
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Timeline } from '../../components/Timeline';
 
 test('renders Timeline', () => {
-    render(<Timeline />);
-    const timelineElement = screen.getByText("Timeline");
-    expect(timelineElement).toBeInTheDocument();
+    render(
+        <Router>
+            <Timeline />
+            const timelineElement = screen.getByText("Timeline");
+            expect(timelineElement).toBeInTheDocument();
+        </Router>
+    );
 });
