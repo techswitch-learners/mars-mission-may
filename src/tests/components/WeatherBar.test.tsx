e4d1a6899be17ec99e41fecacce947abf0211a9c
Cress <cressida.chesworth@softwire.com>
2021-06-25 11:51:39 +0100

    got rid of router in tests

--- src/tests/components/WeatherBar.test.tsx
+++ src/tests/components/WeatherBar.test.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import { WeatherBar } from "../../components/WeatherBar";
-import { BrowserRouter as Router } from "react-router-dom";
 
 test("renders weather bar", () => {
     render( <WeatherBar />);
     const weatherBarElement = screen.getByTestId("weather-banner");
-    
+
     expect(weatherBarElement).toBeInTheDocument();
 });
